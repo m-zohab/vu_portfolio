ffc658ba77d5518bbfa9325c2f1ce736b2d138a9
Zohaib <muhammadzohaib4042@gmail.com>
2026-09-17 12:58:39 +0500

    Refactor App component layout with gradient background

--- src/App.jsx
+++ src/App.jsx
@@ -9,17 +9,23 @@ import WhatsAppButton from "./components/WhatsAppButton.jsx";
 
 export default function App() {
   return (
-    <div className="min-h-screen bg-white">
-      <Navbar />
-      <main>
-        <Hero />
-        <About />
-        <Services />
-        <Portfolio />
-        <Contact />
-      </main>
-      <Footer />
-      <WhatsAppButton />
+    <div className="min-h-screen bg-gradient-to-br from-slate-50 via-white to-blue-50 relative overflow-hidden">
+      {/* Background Pattern */}
+      <div className="absolute inset-0 bg-[radial-gradient(#e2e8f0_1px,transparent_1px)] [background-size:20px_20px] opacity-60"></div>
+      
+      {/* Content */}
+      <div className="relative z-10">
+        <Navbar />
+        <main>
+          <Hero />
+          <About />
+          <Services />
+          <Portfolio />
+          <Contact />
+        </main>
+        <Footer />
+        <WhatsAppButton />
+      </div>
     </div>
   );
 }
